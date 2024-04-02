806e6d980cc0a787e194bb7f763683b2d2f25038
harsh <harsh@webbuddy.agency>
2024-04-02 07:39:27 -0700

    HERO PAGE DONE

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,35 +2,46 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center gap-5">
       <div>
-        <h4>Domain Name Search</h4>
+        <video height="452" width="768" autoPlay  >
+          <source src="/video.mp4" />
+          Your browser does not support the video tag...
+        </video>
       </div>
       <div>
-        <h2>Find Your Dream Domain Name</h2>
+        <h4 className="text-orange-500 font-bold italic text-xl">Domain Name Search</h4>
+      </div>
+      <div className="">
+        <h2 className="font-extrabold text-7xl">Find Your Dream Domain Name</h2>
       </div>
 
       <div>
-        <h4>
+        <h4  className="font-bold mb-10">
           Use our domain checker tool to find the perfect name for your online
           project.
         </h4>
       </div>
 
-      <div className="flex justify-center mt-10">
-      <div className="relative">
-        <input
-          type="text"
-          placeholder="Find your dream domain"
-          className="w-96 p-3 rounded-l-full outline-none focus:ring focus:ring-blue-400"
-        />
-        <button className="absolute top-0 right-0 h-full w-28 bg-orange-500 text-white rounded-3xl flex justify-center items-center">
-          Search
-        </button>
-      </div>
-    </div>
+     ddddv
 
-      <div></div>
+      <div>
+       <h2>
+   <span className="text-black font-bold text-xl">   .com </span>
+   <span className="italic text-xs text-gray-800 font-bold"> $25.00 </span>
+   <span className="text-black font-bold">   $19.00 </span>  
+   <span className="text-black font-bold text-xl">   .co </span>
+   <span className="italic text-xs text-gray-800 font-bold"> $25.00 </span>
+   <span className="text-black font-bold">  $15.00</span>  
+   <span className="text-black font-bold text-xl">   .net</span>
+   <span className="italic text-xs text-gray-800 font-bold"> $25.00 </span>
+   <span className="text-black font-bold">   $16.00 </span>  
+
+   <span className="text-black font-bold text-xl">    .store </span>
+   <span className="italic text-xs text-gray-800 font-bold"> $25.00 </span>
+   <span className="text-black font-bold">  $18.00</span>  
+        </h2>
+      </div>
     </div>
   );
 };
