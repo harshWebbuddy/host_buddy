a7de12b1168ff4b7a28604b0db653c78cd7554a4
harsh <harsh@webbuddy.agency>
2024-04-26 01:41:44 -0700

    save changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body className='relative  justify-center items-center mx-auto'>
-        <NavBar /> 
+        {/* <NavBar />  */}
         {children}
         {/* <Footer /> */}
       </body>
--- app/phone/page.tsx
+++ app/phone/page.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import CustomPage from './hero'
 import Rating from './rating'
+import Plan from './plan'
 
 const page = () => {
   return (
     <div>
         <div><CustomPage/></div>
-        <div><Rating/></div>
-    </div>
+        <div><Rating/></div>   
+        <div><Plan/></div>
+         </div>
   )
 }
 
