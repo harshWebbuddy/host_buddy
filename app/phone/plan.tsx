07ae0df3e61d017dd2f4df640ad19fce13d7ef24
harsh9711 <harsh@webbuddy.agency>
2024-04-28 19:35:11 +0530

    done

--- app/phone/plan.tsx
+++ app/phone/plan.tsx
@@ -137,9 +137,9 @@ const Plan = () => {
   
     <div className='plan2boxinner2 mx-auto'> 
     <div className='plan2boxinner22'>
-     <div className='.planbutton border-2 border-black group-hover:bg-[#FF7500]	 rounded-xl p-3 items-center justify-center flex flex-row'>
+     <div className='.planbutton border-2 border-black group-hover:bg-[#FF7500]  rounded-xl p-3 items-center justify-center flex flex-row '>
         <h2 className='text-[16px] font-bold leading-[19.2px] text-black group-hover:text-white '>Choose Plan </h2>
-         <svg className='item-center justify-center mt-0.5 ml-2' width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+         <svg className='item-center justify-center mt-0.5 ml-2 svghover	' width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M5.13672 0.947266L5.69531 0.388672C5.94922 0.160156
  6.33008 0.160156 6.55859 0.388672L11.5098 5.31445C11.7383 
  5.56836 11.7383 5.94922 11.5098 6.17773L6.55859 11.1289C6.33008
@@ -254,76 +254,7 @@ const Plan = () => {
 </div>
     </div>
     </div>
-     {/* <div className='group special'>
-      <div className='plan2box bg-[#F9EFE4] group-hover:bg-[#16191C]'>
-<div className='.plan2boxinner mt-8'>
-    <div className='plan2boxinner1 mx-auto'>
-      <div className=''>  <div>
-            <h2 className='ignite font-mono-sans mx-auto text-black group-hover:text-white'>Ignite</h2>
-        </div>
-        <div>
-<h2 className='from font-mono-sans mx-auto text-black group-hover:text-white '>From</h2>
-        </div>
-        </div>
-        <div className=''>  <div>
-            <h2 className=' font-mono-sans mx-auto number'>$29</h2>
-        </div>
-        <div>
-<h2 className='from font-mono-sans mx-auto text-black group-hover:text-white'>/Month</h2>
-        </div>
-        </div>
-    </div>
-  
-    <div className='plan2boxinner2 mx-auto'> 
-    <div className='plan2boxinner22'>
-     <div className='.planbutton border-2 border-black group-hover:bg-[#FF7500]	 rounded-xl p-3 items-center justify-center flex flex-row'>
-        <h2 className='text-[16px] font-bold leading-[19.2px] text-black group-hover:text-white '>Choose Plan </h2>
-         <svg className='item-center justify-center mt-0.5 ml-2' width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M5.13672 0.947266L5.69531 0.388672C5.94922 0.160156
- 6.33008 0.160156 6.55859 0.388672L11.5098 5.31445C11.7383 
- 5.56836 11.7383 5.94922 11.5098 6.17773L6.55859 11.1289C6.33008
-  11.3574 5.94922 11.3574 5.69531 11.1289L5.13672 10.5703C4.9082 
-  10.3164 4.9082 9.93555 5.13672 9.68164L8.20898 6.76172H0.921875C0.566406 
-  6.76172 0.3125 6.50781 0.3125 6.15234V5.33984C0.3125 5.00977 0.566406 4.73047 0.921875 4.73047H8.20898L5.13672 1.83594C4.9082 1.58203 4.88281 1.20117 5.13672 0.947266Z" fill="#16191C"/>
-</svg>
-
-</div>
-<div className='features'>
-<div className='text-[16px] font-bold font-mono-sans leading-[24px] text-black group-hover:text-white'><h2>Major Features</h2></div>
-<div className='flex flex-col gap-[10px] '>
-  <div className='subfeatures group'>
-    <h2 className='font-normal text-black group-hover:text-white'>RAM</h2>
-<h2 className='font-semibold text-black group-hover:text-white'>1GB</h2>
-</div>
- <div className='subfeatures group'>
-    <h2 className='font-normal text-black group-hover:text-white'>Processor</h2>
-<h2 className='font-semibold text-black group-hover:text-white'>1 Core</h2>
-</div>
- <div className='subfeatures group'>
-    <h2 className='font-normal text-black group-hover:text-white'>Storage</h2>
-<h2 className='font-semibold text-black group-hover:text-white'>25GB</h2>
-</div>
- <div className='subfeatures group'>
-    <h2 className='font-normal text-black group-hover:text-white'>Bandwidth</h2>
-<h2 className='font-semibold text-black group-hover:text-white'>1TB</h2>
-</div>
-</div>
-
-</div>
-</div>
-<div className='flex flex-row items-center justify-center featuresvg '>
-  <h2 className='text-[16px] font-semibold leading-[24px]  text-[#16191C] group-hover:text-[#FF7500] opacity-45 group-hover:opacity-100 '>See All Features in Details </h2>
-<svg className="ml-4 opacity-45 group-hover:opacity-100"width="12" height="14" viewBox="0 0 9 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M1.51562 13L7.51562 7L1.51562 1" stroke="#16191C" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
-
-
-</div>
-</div>
-
-</div>
-    </div>
-    </div> */}
+    
     
 </div>
 <div className='millions mx-auto'>
