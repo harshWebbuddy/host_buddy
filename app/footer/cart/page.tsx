c7b066a705ac4a2250384b51dc706b144ef57c1c
harsh <harsh@webbuddy.agency>
2024-05-23 02:56:41 -0700

    done

--- app/footer/cart/page.tsx
+++ app/footer/cart/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import HeroHosting from './Hero'
 import PageSecond from './scondpage'
+import Account from './Acount'
 
 const page = () => {
   return (
-    <div className='bg-[#F5F5F5]'>  
-<div><HeroHosting/></div>
-    <div className='pb-80  '>
+    <div className=''>  
+<div className=''><HeroHosting/></div>
+<div className='pt-10'>
+  <Account/>
+</div>
+    <div className='pb-80'>
         <PageSecond/>
     </div>
     </div>
