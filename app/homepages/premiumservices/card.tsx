dfbdd9ff28a4abef1a8007887d19a86eeb1c5487
harsh <harsh@webbuddy.agency>
2024-05-28 03:10:17 -0700

    done

--- app/homepages/premiumservices/card.tsx
+++ app/homepages/premiumservices/card.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div className='mx-auto gapping'>
+    <div className='mx-auto gapping space-y-16 2xl:space-y-0'>
         <div className='slide-reveal wholecard1 mx-auto'>
             
            <div className='cardiv'>
@@ -14,17 +14,16 @@ const Card = () => {
             </div>
           
             <div className="card">
-          <div className="cardinside">
-            <h2 className='display'>Full <span>HD Quality</span>
-<a>Streams <Image src="/ps/watch.svg" alt="watch" width={100} height={100} className='watchcard'/></a></h2>
-            <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          <div className="text-[42px] font-semibold">
+          <h2 className=''>Full <span className='text-orange-500'>HD Quality</span></h2>
+          <div className='flex flex-row'><h2>Streams</h2> <Image src="/ps/watch.svg" alt="watch" width={100} height={100} className='ml-4 watchcard'/></div> 
+            <p className='text-[16px] font-normal'>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
             <a>All Specs</a></p>
          
           </div>
-          <button className="cardbuttonallspecs">All Specs<svg  className="mt-0.5" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M5.13672 0.826172L5.69531 0.267578C5.94922 0.0390625 6.33008 0.0390625 6.55859 0.267578L11.5098 5.19336C11.7383 5.44727 11.7383 5.82812 11.5098 6.05664L6.55859 11.0078C6.33008 11.2363 5.94922 11.2363 5.69531 11.0078L5.13672 10.4492C4.9082 10.1953 4.9082 9.81445 5.13672 9.56055L8.20898 6.64062H0.921875C0.566406 6.64062 0.3125 6.38672 0.3125 6.03125V5.21875C0.3125 4.88867 0.566406 4.60938 0.921875 4.60938H8.20898L5.13672 1.71484C4.9082 1.46094 4.88281 1.08008 5.13672 0.826172Z" fill="#16191C"/>
-</svg>
-</button>
+          <div className="bg-white border-orange-500 border flex flex-row w-44 rounded-xl p-3  items-center justify-center">
+              <h2 className="font-medium text-[20px]  leading-normal ">All Specs</h2> <img src="/hosting/ArrowIcon.svg" className="pl-4 -mt-0.5 "/>
+          </div>
           
         </div>
             
@@ -34,22 +33,21 @@ const Card = () => {
         
            
              <div className="card">
-           <div className="cardinside2">
-       <div className='devices'>      <h2 className='display'>Work on all
+             <div className="text-[42px] font-semibold">
+       <div className=''>      <h2 className=''>Work on all
 
 </h2>
 <span>devices </span></div>
-             <p>Lorem ipsum dolor sit amet consectetur. Amet eu non dignissim commodo convallis mauris. Dui est mauris dictum aliquet justo sodales nulla. At ac vel morbi vestibulum eget lectus faucibus diam.
+             <p className='text-[16px] font-normal'>Lorem ipsum dolor sit amet consectetur. Amet eu non dignissim commodo convallis mauris. Dui est mauris dictum aliquet justo sodales nulla. At ac vel morbi vestibulum eget lectus faucibus diam.
            </p>
           
            </div>
            <div>
             <img src="/ps/card1.svg"/>
            </div>
-           <button className="cardbuttonallspecs2">Devices<svg  className="mt-0.5" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
- <path d="M5.13672 0.826172L5.69531 0.267578C5.94922 0.0390625 6.33008 0.0390625 6.55859 0.267578L11.5098 5.19336C11.7383 5.44727 11.7383 5.82812 11.5098 6.05664L6.55859 11.0078C6.33008 11.2363 5.94922 11.2363 5.69531 11.0078L5.13672 10.4492C4.9082 10.1953 4.9082 9.81445 5.13672 9.56055L8.20898 6.64062H0.921875C0.566406 6.64062 0.3125 6.38672 0.3125 6.03125V5.21875C0.3125 4.88867 0.566406 4.60938 0.921875 4.60938H8.20898L5.13672 1.71484C4.9082 1.46094 4.88281 1.08008 5.13672 0.826172Z" fill="#16191C"/>
- </svg>
- </button>
+           <div className="bg-white border-orange-500 border flex flex-row w-44 rounded-xl p-3  items-center justify-center">
+              <h2 className="font-medium text-[20px]  leading-normal ">Devices</h2> <img src="/hosting/ArrowIcon.svg" className="pl-4 -mt-0.5 "/>
+          </div>
            
          </div>
          <div className='cardiv2'>
