96b463ebb94f3b6a41678c4db2e81ee44891f8c1
harsh <harsh@webbuddy.agency>
2024-06-01 00:40:42 -0700

    git

--- app/homepages/homevideo/plan2.tsx
+++ app/homepages/homevideo/plan2.tsx
@@ -30,7 +30,7 @@ const Plan2 = () => {
     <div className="slide-reveal flex flex-col bl:flex-row justify-between gap-y-6 gap-x-4 items-center mb-8 " >
     <div className="relative w-full">
         <h2 className="text-black font-mona-sans font-semibold text-[32px] 2xl:text-[42px] capitalize leading-relaxed w-full text-center bl:text-left">
-        <span className="pb-4 border-b-4 border-orange-500 text-wite text-black">The Perfe</span>ct - Web Hosting Plan
+        <span className="pb-4 border-b-2 2xl:border-b-4 border-orange-500 text-wite text-black">The Perfe</span>ct - Web Hosting Plan
         </h2>
       </div>
     
