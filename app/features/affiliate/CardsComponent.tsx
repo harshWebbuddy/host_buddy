3f8d99c361078da7c10247e43dfd845df3b01d7c
harsh <harsh@webbuddy.agency>
2024-06-05 22:53:30 -0700

    done

--- app/features/affiliate/CardsComponent.tsx
+++ app/features/affiliate/CardsComponent.tsx
@@ -7,44 +7,44 @@ const CardsComponent = () => {
     <div className="space-y-10 2xl:space-y-28 p-4 max-w-[1300px] mx-auto ">
      
       <div className="slide-reveal w-full flex flex-col xl:flex-row-reverse space-x-2 space-y-16 items-center">
-      <div className="w-full flex justify-center">
+      <div className="w-full 2xl:flex hidden justify-center">
           <div className="relative">
             <Image src="/features/card1.svg" width={500} height={500} alt="" className="rounded-2xl w-full  relative z-10" />
           </div>
         </div>
-        <div className="w-full space-y-6 2xl:space-y-8">
+        <div className="w-full space-y-6 2xl:space-y-10">
           <div className="w-full ">
           <h2 className="text-[40px] 2xl:text-[52px] leading-16 font-medium 2xl:font-bold">Affiliate FAQsn</h2>
           </div>
-          <ul className="w-full ">
-            <li className="flex gap-4 items-start">
+          <div className="w-full ">
             <p className="capitalize leading-6 text-[16px] 2xl:text-[20px] font-light">
-              Check out our FAQs to start your Affiliate business strong.</p>
-            </li>
-         
-          </ul>
+              Check out our FAQs to start your Affiliate business strong.
+              </p>
+          </div>
      
             <div className="bg-white border-orange-500 border  2xl:w-60 p-4 rounded-2xl items-start flex flex-row justify-start">
-                <h2 className="font-medium text-[16px] 2xl:text-[24px] text-left leading-normal ">Get Answers</h2> <img src="/hosting/ArrowIcon.svg" className="w-4 mt-1 2xl:mt-2  ml-6  "/>
+                <h2 className="font-medium text-[16px] 2xl:text-[24px] text-left leading-normal ">Get Answers</h2> <img src="/hosting/ArrowIcon.svg" className="w-4 mt-1 2xl:mt-2.5  ml-6  "/>
             </div>
         </div>
-        
+        <div className="w-full flex 2xl:hidden justify-center">
+          <div className="relative">
+            <Image src="/features/card1.svg" width={500} height={500} alt="" className="rounded-2xl w-full  relative z-10" />
+          </div>
+        </div>
       </div>
       <div className="slide-reveal w-full flex flex-col xl:flex-row-reverse gap-x-32  space-y-16 items-center">
-      <div className="w-full space-y-6 2xl:space-y-8">
+      <div className="w-full space-y-6 2xl:space-y-10">
           <div className="w-full ">
           <h2 className="text-[40px] 2xl:text-[52px]  leading-16 font-medium 2xl:font-bold">Affiliate Assets</h2>
           </div>
-          <ul className="w-full space-y-10">
-            <li className="flex gap-4 items-start">
+          <div className="w-full ">
             <p className="capitalize leading-6 text-[16px] 2xl:text-[20px] font-light">
               Make an impact with our provided visuals and generate sale.</p>
-            </li>
          
-          </ul>
+          </div>
          
-          <div className="bg-white border-orange-500 border  2xl:w-60 p-4 rounded-2xl items-start flex flex-row justify-start">
-              <h2 className="font-medium text-[16px] 2xl:text-[24px] text-left leading-normal ">Check all assets</h2> <img src="/hosting/ArrowIcon.svg" className="w-4 mt-1 2xl:mt-2  ml-6  "/>
+          <div className="bg-white border-orange-500 border  2xl:w-72 p-4 rounded-2xl items-start flex flex-row justify-start">
+              <h2 className="font-medium text-[16px] 2xl:text-[24px] text-left leading-normal ">Check all assets</h2> <img src="/hosting/ArrowIcon.svg" className="w-4 mt-1 2xl:mt-2.5  ml-6  "/>
           </div>
         </div> 
         <div className="w-full flex justify-center">
@@ -55,30 +55,32 @@ const CardsComponent = () => {
        
       
       </div>
-      <div className="slide-reveal w-full flex flex-col xl:flex-row-reverse gap-16 items-center">
-      <div className="w-full flex justify-center">
+      <div className="slide-reveal w-full flex flex-col xl:flex-row-reverse gap-x-32  space-y-16 items-center">
+      <div className="w-full 2xl:flex hidden  justify-center">
           <div className="relative">
             <Image src="/features/card4.svg" width={500} height={500} alt="" className="rounded-2xl  w-full relative z-10" />
           </div>
         </div>
-        <div className="w-full space-y-6 2xl:space-y-8">
+        <div className="w-full space-y-6 2xl:space-y-10">
          <div className="w-full ">
             <h2 className="text-[40px] 2xl:text-[52px]  leading-16 font-medium 2xl:font-bold">Affiliate Program Agreement</h2>
           </div>
            
-          <ul className="w-full space-y-8">
+          <div className="w-full space-y-8">
           
-            <li className="flex gap-4 items-start">
               <p className="capitalize leading-6 text-[16px] 2xl:text-[20px] font-light">
               Ready to join? Read carefully our guidelines for a successful affiliation.</p>
-            </li>
-  
+              </div>
           <div className="bg-white border-orange-500 border  2xl:w-96 p-4 rounded-2xl items-start flex flex-row justify-start">
-              <h2 className="font-medium text-[16px] 2xl:text-[24px] text-left leading-normal ">Read affiliate agreement</h2> <img src="/hosting/ArrowIcon.svg" className="w-4 mt-1 2xl:mt-2  ml-6  "/>
+              <h2 className="font-medium text-[16px] 2xl:text-[24px] text-left leading-normal ">Read affiliate agreement</h2> <img src="/hosting/ArrowIcon.svg" className="w-4 mt-1 2xl:mt-2.5  ml-6  "/>
           </div>
-          </ul>
+         
         </div>
-       
+        <div className="w-full 2xl:hidden flex justify-center">
+          <div className="relative">
+            <Image src="/features/card4.svg" width={500} height={500} alt="" className="rounded-2xl  w-full relative z-10" />
+          </div>
+        </div>
       </div>
    
     </div>
--- app/features/affiliate/page.tsx
+++ app/features/affiliate/page.tsx
@@ -8,6 +8,7 @@ import Faqs from './Faqs'
 import Support from './plan2'
 import CardsComponent from './CardsComponent'
 import { NavBar } from '@/components'
+import Ratingpanel2 from './Ratingpanel'
 
 const page = () => {
   return (
@@ -19,16 +20,17 @@ const page = () => {
         {/* rating panel  */}
         <section>
         <div className="space-y-10 2xl:space-y-32 ">
-          <Ratingpanel/>
-          <Plans/>
+        <div className="2xl:flex xl:flex bl:flex lg:flex md:flex hidden"><Ratingpanel2/></div> 
+        <div className="p-4 2xl:hidden xl:hidden bl:hidden lg:hidden md:hidden flex"> <Ratingpanel /></div> 
+          <Plans />
         </div>
-        </section>
+      </section>
 
         {/* plan page */}
         <div className='pt-16 2xl:pt-40'>  
       <Support/>
         </div>
-        <section className='pt-16 2xl:pt-40 pb-80'>
+        <section className='pt-0 2xl:pt-40 pb-56 2xl:pb-80'>
   <CardsComponent/>
 </section>
      
